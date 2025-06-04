e721cf81b19a105b8aeedb4a2c226cffa3d46c1f
Carlos A. Locatelli J. <call.jmail@gmail.com>
2025-06-04 10:07:33 -0300

    Formulário de Modelos de Despacho 100% funcional

--- Viveiro_Analises/controllers/despachos.py
+++ Viveiro_Analises/controllers/despachos.py
@@ -26,11 +26,49 @@ def Gerenciar_templates(): #Menu
     elif f=='ver':
         form = SQLFORM(db[table], registro, readonly=True, ) 
     else:
-        db[table].id.requires = IS_NOT_IN_DB(db, f'{table}.id', error_message='Já está registrado.')
         form = SQLFORM(db[table], submit_button=f'Registrar {tablename}')
         
-     
-    # Modified buttons with AJAX functionality
+    
+    if form.process().accepted:
+        session.flash = f'Dados atualizados' if registro else 'Registrado'
+        redirect(URL(request.controller, request.function , extension='', args=[form.vars.id], vars={'f':'ver'})) # type: ignore
+    elif form.errors:
+        response.flash = 'Corrija os Erros indicados'
+    else:
+        pass
+    
+    formbusca = buscador('despacho_template', 
+                        nome={'label': 'Nome'},
+                        texto={'label': 'Texto'},
+                        descricao={'label': 'Descrição'})
+
+    return dict(form=form, formbusca=formbusca, registro=registro)
+
+
+
+def form_condicoes():
+    
+    
+    form_condicoes = SQLFORM.factory(
+    Field('campo', requires=IS_IN_SET([campo  if not campo.startswith('_') else '' for campo in list(db.Requerimentos.keys())[26:]] )),
+    Field('operador', requires=IS_IN_SET(["=", "!=", "<", ">", "<=", ">=", "contêm", "está entre", "não está entre", ])),
+    Field('valor'), buttons=[BUTTON('Inserir Condição', type='button', 
+                    _onclick=f'''
+                        var campo = jQuery('#no_table_campo').val();
+                        var operador = jQuery('#no_table_operador').val();
+                        var valor = jQuery('#no_table_valor').val();
+                        var textarea = jQuery('#despacho_template_condicoes')[0];
+                        var startPos = textarea.selectionStart;
+                        var endPos = textarea.selectionEnd;
+                        var text = textarea.value;
+                        var fstring =  "{{" + '"campo": ' + '"' + campo + '"' + ', ' + '"operador": ' + '"' + operador + '"' + ', ' + '"valor": ' + '"' + valor + '"' + "}}"
+                        textarea.value = text.substring(0, startPos) + fstring + "," + '\\n' + text.substring(endPos);
+                        textarea.focus();
+                        return false;
+                    ''', _class='btn btn-info' )], fromstyle= 'inline'
+                                )
+    
+        # Modified buttons with AJAX functionality
     btns_vars = []
     for field in list(db.Requerimentos.keys())[26:]:
         if not field.startswith('_') and not 'qtd_' in field and not 'especie_' in field and not'ALL' in field:
@@ -48,40 +86,4 @@ def Gerenciar_templates(): #Menu
                         ''', _class='btn btn-info')
             btns_vars.append(TD(btn))
     
-    if form.process().accepted:
-        session.flash = f'Dados atualizados' if registro else 'Registrado'
-        redirect(URL(request.controller, request.function , extension='', args=[form.vars.id], vars={'f':'ver'})) # type: ignore
-    elif form.errors:
-        response.flash = 'Corrija os Erros indicados'
-    else:
-        pass
-    
-    formbusca = buscador('despacho_template', 
-                        nome={'label': 'Nome'},
-                        texto={'label': 'Texto'},
-                        descricao={'label': 'Descrição'})
-
-    return dict(form=form, formbusca=formbusca, registro=registro, btns_vars=TABLE(TR(btns_vars[:11]), TR(btns_vars[11:22]), TR(btns_vars[22:])))
-
-
-
-def form_condicoes():
-    form_condicoes = SQLFORM.factory(
-    Field('campo', requires=IS_IN_SET([campo for campo in db.Requerimentos.fields])),
-    Field('operador', requires=IS_IN_SET(["=", "!=", "<", ">", "<=", ">=", "contêm", "está entre", "não está entre", ])),
-    Field('valor'), buttons=[BUTTON('Inserir Condição', type='button', 
-                    _onclick=f'''
-                        var campo = jQuery('#no_table_campo').val();
-                        var operador = jQuery('#no_table_operador').val();
-                        var valor = jQuery('#no_table_valor').val();
-                        var textarea = jQuery('#despacho_template_condicoes')[0];
-                        var startPos = textarea.selectionStart;
-                        var endPos = textarea.selectionEnd;
-                        var text = textarea.value;
-                        var fstring =  "{{" + '"campo": ' + '"' + campo + '"' + ', ' + '"operador": ' + '"' + operador + '"' + ', ' + '"valor": ' + '"' + valor + '"' + "}}"
-                        textarea.value = text.substring(0, startPos) + fstring + "," + '\\n' + text.substring(endPos);
-                        textarea.focus();
-                        return false;
-                    ''', _class='btn btn-info' )], fromstyle= 'bootstrap3_stacked'
-                                )
-    return dict(form_condicoes=form_condicoes)
+    return dict(form_condicoes=form_condicoes, btns_vars=TABLE(TR(btns_vars[:8]), TR(btns_vars[8:16]), TR(btns_vars[16:])))
